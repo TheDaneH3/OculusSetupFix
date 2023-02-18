bf22845c768281d6dbf1cbc589abe1a8d0dbecec
Dane C <dhcdm6@missouri.edu>
2023-02-18 01:34:23 -0600

    Reduce console output and add warning

--- OculusPathFix.ps1
+++ OculusPathFix.ps1
@@ -15,7 +15,7 @@ $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 # Check if the "Oculus VR, LLC" key exists under HKEY_CURRENT_USER\SOFTWARE
 if (!(Test-Path "HKCU:\SOFTWARE\Oculus VR, LLC")) {
     # Create the "Oculus VR, LLC" key under HKEY_CURRENT_USER\SOFTWARE
-    New-Item -Path "HKCU:\SOFTWARE" -Name "Oculus VR, LLC"
+    New-Item -Path "HKCU:\SOFTWARE" -Name "Oculus VR, LLC"  | Out-Null
     Write-Host "Key 'Oculus VR, LLC' created.`n"
 }
 else {
@@ -25,7 +25,7 @@ else {
 # Check if the "Oculus" key exists under "Oculus VR, LLC"
 if (!(Test-Path "HKCU:\SOFTWARE\Oculus VR, LLC\Oculus")) {
     # Create the "Oculus" key under "Oculus VR, LLC"
-    New-Item -Path "HKCU:\SOFTWARE\Oculus VR, LLC" -Name "Oculus"
+    New-Item -Path "HKCU:\SOFTWARE\Oculus VR, LLC" -Name "Oculus"  | Out-Null
     Write-Host "Key 'Oculus' created.`n"
 }
 else {
@@ -35,7 +35,7 @@ else {
 # Check if the "Libraries" key exists under "Oculus"
 if (!(Test-Path "HKCU:\SOFTWARE\Oculus VR, LLC\Oculus\Libraries")) {
     # Create the "Libraries" key under "Oculus"
-    New-Item -Path "HKCU:\SOFTWARE\Oculus VR, LLC\Oculus" -Name "Libraries"
+    New-Item -Path "HKCU:\SOFTWARE\Oculus VR, LLC\Oculus" -Name "Libraries"  | Out-Null
     Write-Host "Key 'Libraries' created.`n"
 }
 else {
@@ -44,8 +44,8 @@ else {
 
 # Script Complete
 Write-Host "Script Complete!`n"
-Write-Host "(!) Please restart the Oculus software to apply changes, and then continue the setup process as usual.`n"
+Write-Warning "Please restart the Oculus software to apply changes."
 
 # Pause at the end of the script
-Write-Host "Press any key to continue..."
+Write-Host "`nPress any key to continue..."
 $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
