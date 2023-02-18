7252381048baf7d4fca8123942ec1a4d6dd0a282
Dane C <24963046+TheDaneH3@users.noreply.github.com>
2023-02-18 00:46:23 -0600

    Add script via upload

---
+++ OculusPathFix.ps1
@@ -0,0 +1,49 @@
+<#
+.NOTES
+   Author      : Dane C.
+   GitHub      : https://github.com/TheDaneH3
+#>
+
+# Inform user of what the script does
+Write-Host "`nThis script will create the necessary registry keys to fix the Oculus OVR88948175 setup issue.`n"
+
+# Wait for the user to input a key to continue
+Write-Host "Press any key to continue...`n"
+$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+# Check if the "Oculus VR, LLC" key exists under HKEY_CURRENT_USER\SOFTWARE
+if (!(Test-Path "HKCU:\SOFTWARE\Oculus VR, LLC")) {
+    # Create the "Oculus VR, LLC" key under HKEY_CURRENT_USER\SOFTWARE
+    New-Item -Path "HKCU:\SOFTWARE" -Name "Oculus VR, LLC"
+    Write-Host "Key 'Oculus VR, LLC' created.`n"
+}
+else {
+    Write-Host "Key 'Oculus VR, LLC' already exists. Skipping creation.`n"
+}
+
+# Check if the "Oculus" key exists under "Oculus VR, LLC"
+if (!(Test-Path "HKCU:\SOFTWARE\Oculus VR, LLC\Oculus")) {
+    # Create the "Oculus" key under "Oculus VR, LLC"
+    New-Item -Path "HKCU:\SOFTWARE\Oculus VR, LLC" -Name "Oculus"
+    Write-Host "Key 'Oculus' created.`n"
+}
+else {
+    Write-Host "Key 'Oculus' already exists. Skipping creation.`n"
+}
+
+# Check if the "Libraries" key exists under "Oculus"
+if (!(Test-Path "HKCU:\SOFTWARE\Oculus VR, LLC\Oculus\Libraries")) {
+    # Create the "Libraries" key under "Oculus"
+    New-Item -Path "HKCU:\SOFTWARE\Oculus VR, LLC\Oculus" -Name "Libraries"
+    Write-Host "Key 'Libraries' created.`n"
+}
+else {
+    Write-Host "Key 'Libraries' already exists. Skipping creation.`n"
+}
+
+# Script Complete
+Write-Host "Script Complete`n"
+
+# Pause at the end of the script
+Write-Host "Press any key to continue..."
+$null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
